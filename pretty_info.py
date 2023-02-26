80088b63b2b05a096d246b6b57f96d03679579f6
Ilya Pankratov <pankratovilyasergeevich@gmail.com>
2023-02-26 19:08:23 +0200

    Added docstrings for class 'SborkaInfo' and all their methods.

--- pretty_info.py
+++ pretty_info.py
@@ -7,6 +7,11 @@ from rich.table import Table
 
 
 class SborkaInfo:
+    '''Класс для красивого вывода информации о конкретной сборке в терминал.
+    Вся информация помещается в панель с заголовком имени сборки.
+    Внутри панели содержится сетка из 1го столбца, первая строка сетки - панель в которой указана плотность сборки.
+    Далее идут строки в которых содержаться объекты деревья с указанными перечнями файлов.'''
+
     tree_phrases = [Text('Список файлов плотностью 80 г/м2:', style='magenta bold'),
                     Text('Список более плотных макетов:', style='magenta'),
                     Text('Внимание, присутствуют файлы Raflatak:', style='cyan'),
@@ -19,6 +24,7 @@ class SborkaInfo:
                     denser_files: list,
                     raflatak_files: list,
                     incorrect_files: list) -> list:
+        '''Строит объект дерево для переданного списка.'''
 
         trees = list()
         for indx, lst in enumerate([ofset_files, denser_files, raflatak_files, incorrect_files]):
@@ -26,12 +32,14 @@ class SborkaInfo:
             for obj in lst:
                 tree.add(obj)
             trees.append(tree)
-            tree = None
 
         return trees
 
     @classmethod
     def build_table(cls, sborka_density: int, trees: list) -> Table:
+        '''Строит сетку из 1 колонки, в которую помещает плотность сборки - панель.
+        А так же списки из файлов, если присутствуют - объекты деревья.'''
+
         table = Table.grid(expand=True)
         table.add_column()
         table.add_row(Panel(f'Плотность сборки - {sborka_density} г/м2', border_style='red'))
@@ -59,6 +67,15 @@ class SborkaInfo:
                    denser_files: list,
                    raflatak_files: list,
                    incorrect_files: list) -> None:
+        '''Выводит в консоль информацию о сборке:
+        - В обязательном порядке:
+          -- name - имя сборки
+          -- density - плотность сборки
+        - Опционально, если присутствуют:
+          -- ofset_files - список файлов 80 г/м2
+          -- denser_files - список более плотных файлов
+          -- raflatak_files - список файлов Raflatak
+          -- incorrect_files - список не корректных файлов.'''
 
         trees = cls.build_trees(ofset_files=ofset_files,
                                 denser_files=denser_files,
